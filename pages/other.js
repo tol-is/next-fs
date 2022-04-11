d44505b38c77863474ebfb89c0a570e35a141128
Apostolos Christodoulou <a7sc11u@gmail.com>
2022-04-11 23:23:58 +0100

    avoid promise.all

--- pages/other.js
+++ pages/other.js
@@ -1,11 +1,11 @@
-import { getContent } from "../lib/lib.js";
+import { getAsyncFile } from "../lib/lib.js";
 
 export default function Other(props) {
   return <main>Other: {JSON.stringify(props)}</main>;
 }
 
 export const getServerSideProps = async ({ params }) => {
-  const data = await getContent();
+  const data = await getAsyncFile();
 
   return {
     props: {

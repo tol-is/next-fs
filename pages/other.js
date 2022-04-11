30fd2d192476b7558149905cbdbdd343e28dcf31
Apostolos Christodoulou <a7sc11u@gmail.com>
2022-04-11 23:26:55 +0100

    idk

--- pages/other.js
+++ pages/other.js
@@ -1,11 +1,11 @@
-import { getAsyncFile } from "../lib/lib.js";
+import { getContent } from "../lib/lib.js";
 
 export default function Other(props) {
   return <main>Other: {JSON.stringify(props)}</main>;
 }
 
 export const getServerSideProps = async ({ params }) => {
-  const data = await getAsyncFile();
+  const data = await getContent();
 
   return {
     props: {

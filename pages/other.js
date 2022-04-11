7309d1be1550f9870336a1e1eed26056336605a9
Apostolos Christodoulou <a7sc11u@gmail.com>
2022-04-11 23:22:20 +0100

    use fs in both routes

--- pages/other.js
+++ pages/other.js
@@ -1,15 +1,15 @@
-// import { getContent } from "../lib/lib.js";
+import { getContent } from "../lib/lib.js";
 
 export default function Other(props) {
   return <main>Other: {JSON.stringify(props)}</main>;
 }
 
 export const getServerSideProps = async ({ params }) => {
-  // const data = await getContent();
+  const data = await getContent();
 
   return {
     props: {
-      // data,
+      data,
     },
   };
 };
